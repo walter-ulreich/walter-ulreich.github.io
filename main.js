195a26e3eaeded75bdde4e9142ec06433be272ba
Walter Ulreich <walterulreich@walters-air.attlocal.net>
2023-12-31 14:07:12 -0600

    update mobile view

--- main.js
+++ main.js
@@ -3,11 +3,17 @@ const swiper = new Swiper('.swiper', {
     
     pagination: {
       el: '.swiper-pagination',
+      clickable: 'true'
     },
   
     navigation: {
       nextEl: '.swiper-button-next',
       prevEl: '.swiper-button-prev',
+    },
+
+    lazy: true,
+    a11y: {
+        enabled: true
     },
   
   });
